7acd8490b5122de0d8b0e39e8d755cd1a073a8a6
dci-student <simaafshar1@gmail.com>
2023-02-01 09:05:05 +0100

    a

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
+ 
 // import "./css/style.css"; //importing css
+const cryptoSelect = document.querySelector(".cryptoSelect");
+const inputAmount = document.querySelector("#inputAmount");
+const currency = document.querySelector(".fiatCurrencySelect");
+const outputAmount= document.querySelector(".form-control");
+const convert = document.querySelector(".btn");
+const form = document.querySelector('form')
 
+// // choose crypto and currency
+// cryptoSelect.addEventListener('change', compute);
+// currency.addEventListener('change', compute);
+
+// // input and output
+// inputAmount
+
+ async function fetchConvertor (e){
+    e.preventDefault()
+   try{
+        let response = await fetch(`https://api.coinconvert.net/convert/${cryptoSelect.value}/${currency.value}?amount=${inputAmount.value}`)
+    let data = await response.json();
+    console.log(data)
+    outputAmount.value = data[currency.value.toUpperCase()]
+    }
+    catch(err) {
+        console.log(err);
+    }
+ }
+
+ form.addEventListener('submit', fetchConvertor)
